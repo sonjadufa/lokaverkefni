cae299d5ee6f5c41caadea470ae6b9ac6d7fe566
Sonja Dufa <sonja@munus.is>
2017-11-29 10:02:24 +0000

    third commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="app">
-  <input type="text" name="title" v-model="task">
-  <button @click="taskInput">Bæta við</button>
-    <div id="tasks" v-for="task in tasks">
-      <ul>
-        <li>{{ task.title }}</li>
-      </ul>
-      
-    </div>
-  
+    <input type="text" name="title" v-model="task">
+    <button @click="taskInput">Bæta við</button>
+      <div id="tasks" v-for="task in tasks">
+        <ul>
+          <label><input type="checkbox" name="checkbox" id="checkbox" @click="taskStatus"> {{ task.title }} </label>
+          
+        </ul>
+      </div>
   </div>
 </template>
 
@@ -20,6 +19,7 @@ export default {
     return {
       tasks: [],
       task: ''
+      
     }
   },
 
@@ -27,42 +27,53 @@ export default {
     var self = this;
     axios.get('http://fjolbraut.org/api/tasks', {
       params: {
-  
-
         api_token: 'G0h6P9g4NcH94VsGg8CHTeqfJYH1YikmZwXHbJHXmBuogVaNxQdT6ZKlyOwc'
-          }
-        })
+        }
+      })
 
-        
-        .then(function(response) {
-          self.tasks = response.data
-          console.log(response);
-          })
+      .then(function(response) {
+        self.tasks = response.data
+        console.log(response);
+      })
      .catch(function(error) {
-          console.log(error);
-        });
+        console.log(error);
+      });
   },
+  
   methods: {
     taskInput: function() {
       axios.post('http://fjolbraut.org/api/tasks?api_token=G0h6P9g4NcH94VsGg8CHTeqfJYH1YikmZwXHbJHXmBuogVaNxQdT6ZKlyOwc', {
             title: this.task
          })
-         .then(function(status) {
-          self.tasks = status.data
-          console.log(status)
+         .then(function(response) {
+          location.reload();
 
           
          })
          .catch(function(error) {
             console.log(error);
          });
+
+
       //this.tasks.push(this.task);
       //this.task = ''
-    }
+    }, 
+
+    taskStatus(id){
+    
+
+    axios.post('http://fjolbraut.org/api/tasks/' + id + '/status?api_token=G0h6P9g4NcH94VsGg8CHTeqfJYH1YikmZwXHbJHXmBuogVaNxQdT6ZKlyOwc')
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+         });
+      }
   }
 }
 </script>
 
-<style lang="scss">
+<style>
 
 </style>
